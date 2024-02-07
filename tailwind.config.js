91c23db6f94c39f1ec879d7d7672b508b59f8426
Enoch <enoch@cydef.ca>
2024-02-07 15:18:15 -0500

    initail commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx}', './index.html'],
+  theme: {
+    extend: {
+      colors: {
+        brand: '#FF0000',
+      },
+    },
+  },
+  plugins: [],
+};
